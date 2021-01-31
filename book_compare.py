9ea7d8a14587d268f71da7d7f530fb7187fcb6bc
pizzadragonheart913 <1771364@hansung.ac.kr>
2021-01-31 15:15:37 +0900

    commit

---
+++ book_compare.py
@@ -0,0 +1,19 @@
+#연산자 오버로딩으로 책 페이지 비교
+
+class book:
+    title = ""
+    page = 0
+
+    def __init__(self, title, page):
+        self.title = title
+        self.page = page
+
+    def __str__(self):
+        return self.title
+
+    def __gt__(self, other):
+        return self.page > other.page
+
+book1 = book("magic of python", 700)
+book2 = book("master of the python", 600)
+print(book1 > book2)
