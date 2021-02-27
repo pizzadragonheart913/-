31d59061d4d079072bc1aec6ba6cc91b84f5fd01
pizzadragonheart913 <1771364@hansung.ac.kr>
2021-02-27 18:48:36 +0900

    commmit

--- 2562.py
+++
@@ -1,7 +0,0 @@
-emptylist = []
-
-for i in range(0,9,1):
-    emptylist.append(int(input()))
-
-print(max(emptylist))
-print(emptylist.index(max(emptylist))+1)
--- 2577.py
+++ 2577.py
@@ -4,7 +4,7 @@ C = int(input())
 intanswer = A*B*C
 answer = str(intanswer)
 
-for i in range(0,9,1):
+for i in range(10):
     result = 0
     for j in range(len(answer)):
         if(i==int(answer[j])):
